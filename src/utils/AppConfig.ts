3760f99904cbe1cde8573f8df8d09735bacc7ad0
Alexander Franco <85953377+AlexandeerFG@users.noreply.github.com>
2023-10-06 13:06:14 -0600

    ajustus 2

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,7 +1,7 @@
 export const AppConfig = {
   site_name: 'Starter',
   title: 'Este es un esqueleto',
-  description: 'Aqui va a mi mi codigo by Alets. Lo vera Karlita < 3',
+  description: '6 oct 13:05 Alex',
   url: 'https://example.com',
   locale: 'en',
   author: 'Anonymous',

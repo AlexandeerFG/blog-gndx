11a01be73ad16eb933e385d4af74f48a26b305c6
Alexander Franco <85953377+AlexandeerFG@users.noreply.github.com>
2023-10-06 12:31:01 -0600

    ajustes

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,8 +1,7 @@
 export const AppConfig = {
   site_name: 'Starter',
-  title: 'Next.js Boilerplate',
-  description:
-    'Starter code for your Next.js blog Boilerplate with Tailwind CSS',
+  title: 'Este es un esqueleto',
+  description: 'Aqui va a mi mi codigo by Alets. Lo vera Karlita < 3',
   url: 'https://example.com',
   locale: 'en',
   author: 'Anonymous',
